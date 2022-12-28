9b98eac87b3f09b314ba4b55d7f84ecf3f1d797f
thaishoaf <20520510@gm.uit.edu.vn>
2022-12-28 10:48:36 +0700

    changePassword

--- IE307-client/IE307/Views/ProductsDetailPage.xaml.cs
+++ IE307-client/IE307/Views/ProductsDetailPage.xaml.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using IE307.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace IE307.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -16,5 +17,14 @@ namespace IE307.Views
         {
             InitializeComponent();
         }
+
+        int favouriteTapCount = 0;
+
+        //private void ImgAddToWishlist_Tapped(object sender, EventArgs e)
+        //{
+        //    favouriteTapCount++;
+        //    Image img = sender as Image;
+        //    img.Source = favouriteTapCount % 2 == 0 ? "heart.png" : "redheart.png";
+        //}
     }
 }
