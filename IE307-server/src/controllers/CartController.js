a5f6442bd21ece32e735f44027eb7bebf05f349c
= <=>
2022-12-28 16:05:33 +0700

    API for cart page

--- IE307-server/src/controllers/CartController.js
+++ IE307-server/src/controllers/CartController.js
@@ -105,6 +105,136 @@ class CartController{
             }
         })
     }
+
+    //[DELETE] cart/delete
+    async delete(req, res){
+        const userID=req.body.userID;
+        const productID=req.body.productID;
+        const product=await Products.findById(productID);
+        if(product){
+            Cart.findOne({userID: userID},(err,cart)=>{
+                if(cart){
+                    const cartID=cart.id;
+                    let itemID, itemQuanity;
+                    cart.items.forEach(sp => {
+                        if(sp.item.id==productID){
+                            itemID=sp.id;
+                            itemQuanity=sp.quantity;
+                        }
+                    });
+                    Cart.findOneAndUpdate({_id: cartID, items: {$elemMatch: {item: product}}},
+                        {$pull: {items: {_id: itemID }},
+                        $inc: {totalPrice: -(product.price*itemQuanity), totalQuantity: -itemQuanity}}
+                        ,(err,gioHang)=>{
+                            if(gioHang){
+                                return res.status(200).send("Đã xóa sản phẩm thành công");
+                            }
+                            else if(!gioHang){
+                                return res.status(400).send("Không tìm thấy sản phẩm");
+                            }
+                            else{
+                                return res.status(500).send("Lỗi server");
+                            }
+                        })
+                }
+                else if(!cart){
+                    return res.status(404).send("Không tìm thấy giỏ hàng");
+                }
+                else{
+                    return res.status(500).send("Lỗi server");
+                }
+            })
+        }
+        else{
+            return res.status(500).send("Sản phẩm không tồn tại");
+        }
+    }
+
+    //[PUT] cart/inc
+    async inc(req,res){
+        const userID=req.body.userID;
+        const productID=req.body.productID;
+        const product=await Products.findById(productID);
+        if(product){
+            Cart.findOne({userID: userID},(err,cart)=>{
+                if(cart){
+                    const cartID=cart.id;
+                    let itemID, itemQuanity;
+                    cart.items.forEach(sp => {
+                        if(sp.item.id==productID){
+                            itemID=sp.id;
+                            itemQuanity=sp.quantity;
+                        }
+                    });
+                    Cart.findOneAndUpdate({_id: cartID, items: {$elemMatch: {item: product}}},
+                        {$inc: {totalPrice: product.price, totalQuantity: 1, 'items.$.quantity': 1, 'items.$.price': product.price}}
+                        ,(err,gioHang)=>{
+                            if(gioHang){
+                                return res.status(200).send("Đã tăng số lượng sản phẩm thành công");
+                            }
+                            else if(!gioHang){
+                                return res.status(400).send("Không tìm thấy sản phẩm");
+                            }
+                            else{
+                                return res.status(500).send("Lỗi server");
+                            }
+                        })
+                }
+                else if(!cart){
+                    return res.status(404).send("Không tìm thấy giỏ hàng");
+                }
+                else{
+                    return res.status(500).send("Lỗi server");
+                }
+            })
+        }
+        else{
+            return res.status(500).send("Sản phẩm không tồn tại");
+        }
+    }
+
+    //[PUT] cart/reduce
+    async reduce(req, res){
+        const userID=req.body.userID;
+        const productID=req.body.productID;
+        const product=await Products.findById(productID);
+        if(product){
+            Cart.findOne({userID: userID},(err,cart)=>{
+                if(cart){
+                    const cartID=cart.id;
+                    let itemID, itemQuanity;
+                    cart.items.forEach(sp => {
+                        if(sp.item.id==productID){
+                            itemID=sp.id;
+                            itemQuanity=sp.quantity;
+                        }
+                    });
+                    Cart.findOneAndUpdate({_id: cartID, items: {$elemMatch: {item: product}}},
+                        {$inc: {totalPrice: -product.price, totalQuantity: -1, 'items.$.quantity': -1, 'items.$.price': -product.price}}
+                        ,(err,gioHang)=>{
+                            if(gioHang){
+                                return res.status(200).send("Đã giảm số lượng sản phẩm thành công");
+                            }
+                            else if(!gioHang){
+                                return res.status(400).send("Không tìm thấy sản phẩm");
+                            }
+                            else{
+                                return res.status(500).send("Lỗi server");
+                            }
+                        })
+                }
+                else if(!cart){
+                    return res.status(404).send("Không tìm thấy giỏ hàng");
+                }
+                else{
+                    return res.status(500).send("Lỗi server");
+                }
+            })
+        }
+        else{
+            return res.status(500).send("Sản phẩm không tồn tại");
+        }
+    }
 }
 
 module.exports = new CartController();
